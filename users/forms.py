590dc4511c04028b4d663b151291996fc9a481d4
merry-hyelyn <h1335@naver.com>
2020-06-19 22:45:14 +0900

    modify user

--- users/forms.py
+++ users/forms.py
@@ -2,5 +2,24 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
-    model = User
-    feild = ['username', 'password','']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password',]
+        widget = {
+            'username':forms.TextInput(attrs={
+                'class' : 'form-control',
+                'placeholder' : '15자 이내로 입력 가능합니다.',
+            }),
+            'email' : forms.EmailInput(attrs={
+                'class' : 'form-control',
+            }),
+            'password' : forms.PasswordInput(attrs={
+                'class' : 'form-control',
+            }),
+        }
+
+        label = {
+            'username' : '이름',
+            'email' : '이메일',
+            'password' : '비밀번호',
+        }
--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from .forms import UserForm
+from django.http import HttpResponse
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect("login")
+    user_form = UserForm()
+    return render(request, "signup.html", {"user_form": user_form})
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["pwd"]
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("index")
+        else:
+            return render(
+                request, "login.html", {"error": "username or password is incorrect."}
+            )
+    else:
+        return render(request, "login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("index")
+
