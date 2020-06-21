394a5f423f3ba9f30a2fb8c6e5bdf4a33940a81a
merry-hyelyn <h1335@naver.com>
2020-06-21 22:26:40 +0900

    Change board model

--- boards/models.py
+++ boards/models.py
@@ -4,9 +4,9 @@ from core.models import AbstractTimeStamp
 
 
 class Board(AbstractTimeStamp):
-    name = models.CharField(max_length=20) # 게시판 이름
-    create_user = models.OneToOneField(User, on_delete=models.CASCADE) # 게시판 생성자
-    path = models.CharField(max_length=20) # 게시판 url 경로 /hyelyn
+    name = models.CharField(max_length=20)  # 게시판 이름
+    create_user = models.ForeignKey(User, on_delete=models.CASCADE)  # 게시판 생성자
+    path = models.CharField(max_length=20)  # 게시판 url 경로 /hyelyn
 
     def __str__(self):
         return self.name
--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,24 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, reverse
 from boards.forms import BoardForm
+from users.models import User
 
-# Create your views here.
+
+@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url="index")
 def new(request):
-    # board_form = BoardForm()
-    # return render(request, "boards/new.html", {"board_form": board_form})
-    return render(request, "boards/new.html")
+    if request.method == "POST":
+        board_form = BoardForm(request.POST)
+        if board_form.is_valid():
+            board = board_form.save(commit=False)
+            board.create_user = request.user
+            board.save()
 
+        return redirect('index')
+
+    else:
+        board_form = BoardForm()
+        user = request.user
+        return render(request, "boards/new.html", {
+            "board_form": board_form,
+            "user": user,
+        })
