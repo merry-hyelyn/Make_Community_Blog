468acd4d7ffc61a8f51bb12d11358de0c966dc27
merry-hyelyn <h1335@naver.com>
2020-06-26 22:50:59 +0900

    Add model field and migrate

--- users/models.py
+++ users/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    ADMIN = "admin" # 전부 다 가능 superuser
-    STAFF = "staff" # 글 삭제 댓글 삭제 가능 staff
-    NORMAL_USER = "normal" # 글 생성 댓글 생성 가능 just member(user)
-    
+    ADMIN = "admin"  # 전부 다 가능 superuser
+    STAFF = "staff"  # 글 삭제 댓글 삭제 가능 staff
+    NORMAL_USER = "normal"  # 글 생성 댓글 생성 가능 just member(user)
+
     PERMISSION_CHOICES = (
-        (ADMIN, "Admin",),
-        (STAFF, "Staff",),
-        (NORMAL_USER, "Normal",),
+        (
+            ADMIN,
+            "Admin",
+        ),
+        (
+            STAFF,
+            "Staff",
+        ),
+        (
+            NORMAL_USER,
+            "Normal",
+        ),
     )
 
     ONE_STEP = "one"
@@ -18,12 +28,26 @@ class User(AbstractUser):
     STAFF_STEP = "zero"
 
     STEP_CHOICES = (
-        (STAFF_STEP, "Zero",),
-        (ONE_STEP, "One",),
-        (TWO_STEP, "Two",),
-        (THIRD_STEP, "Third",),
+        (
+            STAFF_STEP,
+            "Zero",
+        ),
+        (
+            ONE_STEP,
+            "One",
+        ),
+        (
+            TWO_STEP,
+            "Two",
+        ),
+        (
+            THIRD_STEP,
+            "Third",
+        ),
     )
-    permission = models.CharField(max_length=7, choices=PERMISSION_CHOICES, default=NORMAL_USER)
-    step = models.CharField(max_length=6, choices=STEP_CHOICES, default=ONE_STEP)
-
-    
+    permission = models.CharField(max_length=7,
+                                  choices=PERMISSION_CHOICES,
+                                  default=NORMAL_USER)
+    step = models.CharField(max_length=6,
+                            choices=STEP_CHOICES,
+                            default=ONE_STEP)
